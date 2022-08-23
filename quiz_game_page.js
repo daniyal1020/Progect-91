c6711a238f1b19eb21f3b9d22249f29ee7294660
daniyal1020 <107762034+daniyal1020@users.noreply.github.com>
2022-08-23 18:00:28 +0530

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,21 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+player1_score = 0;
+player2_score = 0;
+document.getElementById("player1_name").innerHTML=player1_name + ":";
+document.getElementById("player2_name").innerHTML=player2_name + ":";
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score").innerHTML=player2_score;
+document.getElementById("player_question").innerHTML="Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML="Answer Turn - " + player2_name;
+
+function submit(){
+    number_1 = document.getElementById("number_1").value
+    number_2 = document.getElementById("number_2").value
+    actual_answer = number_1 * number_2;
+    question_number = "<h4>" + number_1 + "X" + number_2;
+    input_box = "<br> answer : <input type='text' id='input_check_box'";
+    check_button = "<br><br> <button class='btn btn-info' onclick='check'>Check</button>";
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+}
